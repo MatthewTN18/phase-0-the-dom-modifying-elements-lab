1e5c1d491513acdd3e5e5eefb6ed661bfbe00454
Matthew <matthewtndirangu@gmail.com>
2024-04-05 04:46:35 +0300

    feat: complete lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+
+const mainElement = document.getElementById("main");
+mainElement.remove()
+
+const newHeader = document.createElement('h1');
+
+newHeader.setAttribute('id', 'victory')
+
+newHeader.textContent = "MATTHEW is the champion";
